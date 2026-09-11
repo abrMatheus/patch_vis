4d7fe97368c164972de82528cce173abbc0b56c5
AbrMatheus <matheus.abrantesc@gmail.com>
2026-09-11 05:11:25 -0300

    receptive field
    Signed-off-by: AbrMatheus <matheus.abrantesc@gmail.com>

--- 1_save_svox_patches.py
+++ 1_save_svox_patches.py
@@ -6,13 +6,24 @@ from CONFIG import *
 from src.flim_tools import load_patches_from_marked_images
 
 
-def save_patches(res_dir, patches, labels):
+def save_patches_labels(res_dir, labels):
 
-    np.save(f"{res_dir}/not_norm_patches.npy", patches)
     np.save(f"{res_dir}/labels.npy", labels)
 
 
-def gen_patches(dataset, split, n_svox, k_size):
+def save_patches_layer(res_dir, patches, layer):
+    np.save(f"{res_dir}/not_norm_patches_l{layer}.npy", patches)
+
+def compute_receptive_field(layer, strides, k_size):
+    rf = 1
+    jump = 1
+    for l in range(0,layer):
+        rf += (k_size-1)*jump
+        jump *=strides[l]
+
+    return rf
+
+def gen_patches(dataset, split, n_svox, k_size, strides):
 
 
     res_dir = f"s_patches/{dataset}/s_{split}/n_{n_svox}/k_{k_size}"
@@ -32,26 +43,27 @@ def gen_patches(dataset, split, n_svox, k_size):
 
     marker_dir = f"s_markers/{dataset}/s_{split}/n_{n_svox}"
 
-    kernel_size = [k_size,k_size]
-    patches, labels, ilabel = load_patches_from_marked_images(marker_dir, IM_DIR, GT_DIR, kernel_size=kernel_size, use_lab=use_lab)
+    for layer in range(0, len(strides)):
+        rf = compute_receptive_field(layer+1, strides, k_size)
+        kernel_size = [rf,rf]
+        patches, labels, ilabel = load_patches_from_marked_images(marker_dir, IM_DIR, GT_DIR, kernel_size=kernel_size, use_lab=use_lab)
 
-    save_patches(res_dir, patches, labels)
+        if layer == 0:
+            save_patches_labels(res_dir, labels)
+        save_patches_layer(res_dir, patches, layer+1)
 
 
 
 
 
-
-
-stride = [1,2,1]
 if __name__ == "__main__":
 
-    split = 1
-
+    split = 3
+    strides = [1,2,1]
 
     for n_svox in [25]:#50
-        for kernel_size in [3,5,9,11]:
+        for kernel_size in [3,5,9]:
 
             for dataset in ['plants','schisto','refuge', 'fish', 'gbm2d', 'mass_building']:
                 print(f"Getting patches from {dataset} {split} {n_svox} {kernel_size}")
-                gen_patches(dataset, split, n_svox, kernel_size)
+                gen_patches(dataset, split, n_svox, kernel_size, strides)
